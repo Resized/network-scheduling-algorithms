35f1f6bebd998620aaa2d093932e717ede41ef5e
Resized <shai.yarden@gmail.com>
2022-11-27 07:18:16 +0200

    main to sim flow

--- token_bucket.go
+++ token_bucket.go
@@ -33,7 +33,24 @@ func (tb *TokenBucket) sendPacket(p Packet) error {
 	if float64(p.size) > tb.currentTokens {
 		return fmt.Errorf("not enough tokens in bucket, need %v, have %.3v", p.size, tb.currentTokens)
 	}
-	fmt.Printf("sending packet of size %v, current tokens: %.3v\n", p.size, tb.currentTokens)
 	tb.currentTokens -= float64(p.size)
+	fmt.Printf("sending packet of size %v, current tokens: %.3v\n", p.size, tb.currentTokens)
 	return nil
+}
+
+func main() {
+	var p = [10]Packet{}
+	for i := 0; i < 10; i++ {
+		p[i].size = i + 30
+	}
+	var tb = TokenBucket{maxCapacity: 100, currentTokens: 0, fillRate: 20, lastTime: time.Now()}
+	for {
+		for i := 0; i < 10; i++ {
+			err := tb.sendPacket(p[i])
+			if err != nil {
+				fmt.Printf("error sending packet: %v\n", err)
+			}
+			time.Sleep(time.Second)
+		}
+	}
 }
